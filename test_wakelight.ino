7be9d4a93dbb83713e685a31de21811fdcd48abe
Mitchell Ciupak <42656862+mitchellciupak@users.noreply.github.com>
2020-05-18 15:35:34 -0500

    Update test_wakelight.ino

--- test_wakelight.ino
+++ test_wakelight.ino
@@ -4,20 +4,18 @@
 
 #include <FastLED.h>
 
-#define NUM_LEDS 6
-#define LED_PIN 2
-#define FADETIME_MIN 30 
+#define NUM_LEDS 60
+#define DATA_PIN 2
 
-CRGB led(NUM_LEDS);
+CRGB leds[NUM_LEDS];
 
 void setup() {
-  FastLED.addLeds<NEOPIXEL, LED_PIN > (led, NUM_LEDS);
-  
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 }
 
 void setRGB(int red, int green, int blue) {
   for (int i=0; i < NUM_LEDS; i++) {
-    led[i] = CRGB(red,green,blue); //RGB
+    leds[i] = CRGB(red,green,blue); //RGB
   }
   FastLED.show();
 }
